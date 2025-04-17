b33cfcd8f16dc3479e61be7ad18477f681b6834a
Mürsel SEN <murselsen_35@hotmail.com>
2025-04-17 13:38:23 +0300

    feat: implement course addition functionality and refactor App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
-// import { useState } from 'react'
+import React, { useState } from 'react';
 
-import './App.css'
+// Css
+import './App.css';
 
-function App() {
+// Components
+import Course from './Course';
 
-  return (
-    <>
-      <div className="App">
-        React | Vite
-      </div>
-    </>
-  )
+const getRandomCourse = () => {
+    const courseArray = ["Angular", "Bootstrap", "CcSharp", "KompleWeb"];
+    const randomIndex = Math.floor(Math.random() * courseArray.length);
+    return courseArray[randomIndex];
 }
 
-export default App
+
+// Render the App component
+const App = () => {
+    const [courses, setCourses] = useState([]);
+
+    const handleClick = () => {
+        setCourses(
+            [...courses, getRandomCourse()]
+        )
+    };
+
+    const courseList = courses.map((course, index) => {
+        return <Course key={index} courseName={course} />
+
+    });
+
+
+    return (
+        <>
+            <div className='App'>
+                <button onClick={handleClick}>Kurs Ekle</button>
+                <div className='CourseList'>
+                    {courseList}
+                </div>
+            </div>
+        </>
+    );
+};
+
+
+export default App;
